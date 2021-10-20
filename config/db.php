cf8be8d98dfad6da07eeae96a54a20ad3aeb5495
Matvey021292 <raul021292@gmail.com>
2021-10-20 16:29:00 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'matvey',
-    'password' => '15112016',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
